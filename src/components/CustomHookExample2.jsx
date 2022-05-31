64f0072c467c43e37bc0e34bd85c2e1b1e262d5a
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-05-31 14:53:01 -0700

    React Hook Examples

---
+++ src/components/CustomHookExample2.jsx
@@ -0,0 +1,38 @@
+import useLocalStorage from "../hooks/useLocalStorage"
+
+function CustomHookExample2() {
+    const [task, setTask] = useLocalStorage('task', '')
+    const [tasks, setTasks] = useLocalStorage('tasks', [])
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        const taskObj = {
+            task,
+            completed: false,
+            data: new Date().toLocaleDateString()
+        }
+
+        setTasks([...tasks, taskObj])
+    }
+
+    return (
+    <>
+    <form onSubmit={onSubmit} className='w-25'>
+        <div className='mb-3'>
+            <label className='form-label'>Task</label>
+            <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+        </div>
+        <button type="submit" className='btn btn-primary'>Submit</button>
+    </form>
+
+    <hr />
+
+    {tasks.map((task) => (
+        <h3 key={task.date}>{task.task}</h3>
+    ))}
+    </>
+    )
+}
+
+export default CustomHookExample2
